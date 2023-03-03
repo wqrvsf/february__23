7a17a6875f8d0de8c3b0014c4f9ed0f3eda2a764
wqrvsf <98298812+wqrvsf@users.noreply.github.com>
2023-03-03 23:01:09 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let title = document.querySelector('.holiday__title');
 let info = document.querySelector('.holiday__info');
 let btn = document.querySelector('.holiday__btn');
+let block = document.querySelector('.holiday__block');
+let feb = document.querySelector('.holiday__feb');
 
 title.addEventListener('click', function () {
-    title.style.color = 'blue';
-    title.style.fontSize = '105px';
+    feb.style.display = 'none';
+    info.style.display = 'flex';
+});
+
+btn.addEventListener('click', function () {
+    info.style.display = 'none';
+    block.style.display = 'flex';
+
 });
